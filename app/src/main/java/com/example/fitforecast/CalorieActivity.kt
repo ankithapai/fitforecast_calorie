7e93adc98124181919417fee9f35671850c857e7
Ankitha <ankithapai@ymail.com>
2023-11-29 20:32:46 -0700

    calorie prediction and macro nutrients split.

---
+++ app/src/main/java/com/example/fitforecast/CalorieActivity.kt
@@ -0,0 +1,28 @@
+package com.example.fitforecast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
+class CalorieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calorie)
+        val myCalorieView = findViewById<TextView>(R.id.calorieIntake)
+        val myCarbsView = findViewById<TextView>(R.id.carbs_split)
+        val myProteinView = findViewById<TextView>(R.id.protein_split)
+        val myFatView = findViewById<TextView>(R.id.fat_split)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val randomCalories = Random.nextInt(958, 1700)
+            val calcCarb = (randomCalories * 0.45).toInt()
+            val calcProtein = (randomCalories * 0.20).toInt()
+            val calcFat = (randomCalories * 0.35).toInt()
+            myCalorieView.text = "Calories consumed: $randomCalories"
+            myCarbsView.text = "Carbs split: $calcCarb"
+            myProteinView.text = "Protein split: $calcProtein"
+            myFatView.text = "Fat split: $calcFat"
+        }
+    }
+}
